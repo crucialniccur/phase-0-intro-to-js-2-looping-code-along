6e2225a6f5d58d9a540d53b6a39262528a437d35
Mati Joseph <crucialniccur@gmail.com>
2025-03-07 11:35:19 +0300

    comment out all error function code

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
 // Code your solutions in this file
-let names = ["Joes", "Sam", "Borne"];
+let names = [];
 
-function writeCards(name, value) {
-  console.log(`Thank you ${name}, for the wonderful surprise gift!`);
-}
+// function writeCards(input) {
+//   //   let namesCopy = { ...names, input };
+//   //   return `Thank you, ${name}, for the wonderful surprise gift!`;
+//   for (let i = 0; i < names.length; i++) {
+//     console.log(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+//   }
+// }
+
+// writeCards("mati");
+
+// function writeCards(theNames) {
+//   //   let theNames = [];
+
+//   for (let i = 0; i < theNames.length; i++) {
+//     console.log(`Thank you, {theNames[i]}, for the wonderful surprise`);
+//   }
+// }
