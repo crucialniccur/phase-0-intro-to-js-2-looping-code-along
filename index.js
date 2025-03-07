0d76a48d9266b0221a5c53a97e8168a160045b6b
Mati Joseph <crucialniccur@gmail.com>
2025-03-07 10:59:32 +0300

    describe the wrapCards function and create a names array

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Code your solutions in this file
+let names = ["Joes", "Sam", "Borne"];
+
+function wrapCards(name, value) {
+  console.log(`Thank you ${name}, for the wonderful surprise gift!`);
+}
